40078192234a1986f8cb9255545f66b6cb4a0233
Ajaspj <ajaspj123@gmail.com>
2024-08-03 15:05:20 +0530

    updated data fetching for faster data loading

--- lib/providers/cardamomdataprovider.dart
+++ lib/providers/cardamomdataprovider.dart
@@ -20,22 +20,18 @@ class CardamomDataProvider with ChangeNotifier {
   bool get hasMoreData => _hasMoreData;
 
   CardamomDataProvider() {
+    fetchCardamomData();
     _startPeriodicFetch();
-    _initializeData();
   }
 
-  Future<void> _initializeData() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-    if (connectivityResult == ConnectivityResult.none) {
-      print('No internet connection. Loading data from local storage.');
-      await loadDataFromLocal();
-    } else {
-      print('Internet connection available. Fetching initial data from API.');
-      await fetchFromApi(); // Fetch initial data from API
+  Future<void> fetchCardamomData({bool refresh = false}) async {
+    if (refresh) {
+      _currentPage = 1;
+      _hasMoreData = true;
+      _cardamomData.clear();
+      notifyListeners();
     }
-  }
 
-  Future<void> fetchCardamomData() async {
     var connectivityResult = await Connectivity().checkConnectivity();
     if (connectivityResult == ConnectivityResult.none) {
       print('No internet connection. Loading data from local storage.');
@@ -55,18 +51,12 @@ class CardamomDataProvider with ChangeNotifier {
     try {
       List<CardamomData> newData =
           await fetchCardamomDataPage(_currentPage, _perPage);
-      print('Fetched ${newData.length} new items from API');
-
-      if (newData.isNotEmpty) {
-        _cardamomData.addAll(newData);
-        _currentPage++;
-        await _saveDataToLocal(); // Save updated data to local storage
-        print('Added ${newData.length} items to _cardamomData');
-      }
-
       if (newData.length < _perPage) {
         _hasMoreData = false;
       }
+      _cardamomData.addAll(newData);
+      _currentPage++;
+      await _saveDataToLocal();
     } catch (e) {
       print('Error fetching data: $e');
       _hasMoreData = false;
@@ -92,7 +82,8 @@ class CardamomDataProvider with ChangeNotifier {
       _cardamomData =
           jsonData.map((item) => CardamomData.fromJson(item)).toList();
       notifyListeners();
-      print('Loaded ${_cardamomData.length} items from SharedPreferences');
+      print(
+          'Data loaded from SharedPreferences: ${_cardamomData.length} items');
     } else {
       print('No data found in SharedPreferences');
     }
@@ -115,8 +106,8 @@ class CardamomDataProvider with ChangeNotifier {
 
   void _startPeriodicFetch() {
     _fetchTimer = Timer.periodic(Duration(minutes: 5), (timer) async {
-      print('Periodic fetch triggered');
-      await fetchFromApi();
+      print('Fetching new data from API');
+      await fetchCardamomData(refresh: true);
     });
   }
 
--- lib/view/homescreen/homescreen.dart
+++ lib/view/homescreen/homescreen.dart
@@ -48,6 +48,11 @@ class _CardamomScreenState extends State<CardamomScreen> {
     });
   }
 
+  Future<void> _refreshData() async {
+    await Provider.of<CardamomDataProvider>(context, listen: false)
+        .fetchCardamomData(refresh: true);
+  }
+
   Future<void> _shareData(CardamomData data) async {
     final String text = 'Cardamom Data:\n'
         'Auctioneer: ${data.auctioneer}\n'
@@ -86,161 +91,86 @@ class _CardamomScreenState extends State<CardamomScreen> {
           ),
         ],
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          double horizontalPadding = 16.0;
-          double verticalPadding = 8.0;
-          double fontSizeTitle = 18.0;
-          double fontSizeBody = 16.0;
-
-          if (constraints.maxWidth > 1200) {
-            // Tablet and Web
-            horizontalPadding = 32.0;
-            verticalPadding = 16.0;
-            fontSizeTitle = 24.0;
-            fontSizeBody = 20.0;
-          } else if (constraints.maxWidth > 600) {
-            // Tablet
-            horizontalPadding = 24.0;
-            verticalPadding = 12.0;
-            fontSizeTitle = 20.0;
-            fontSizeBody = 18.0;
-          }
-
-          return Consumer<CardamomDataProvider>(
-            builder: (context, provider, child) {
-              if (provider.isLoading && provider.cardamomData.isEmpty) {
-                return Center(
-                  child: Lottie.asset(
-                      "assets/animations/Animation - 1722503461417 (1).json"),
-                );
-              } else if (provider.cardamomData.isEmpty) {
-                return Center(child: Text('No data available'));
-              } else {
-                return RefreshIndicator(
-                  onRefresh: _fetchData,
-                  child: ListView.builder(
-                    controller: _scrollController,
-                    itemCount: provider.cardamomData.length +
-                        (provider.hasMoreData ? 1 : 0),
-                    itemBuilder: (context, index) {
-                      if (index == provider.cardamomData.length) {
-                        return Center(child: CircularProgressIndicator());
-                      }
-                      CardamomData data = provider.cardamomData[index];
-                      return Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: horizontalPadding,
-                            vertical: verticalPadding),
-                        child: Card(
-                          elevation: 6,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: ListTile(
-                            contentPadding: EdgeInsets.all(16),
-                            leading: CircleAvatar(
-                              backgroundColor: Colors.teal,
-                              child:
-                                  Icon(Icons.local_offer, color: Colors.white),
-                            ),
-                            title: Text(
-                              'Auctioneer: ${data.auctioneer}',
-                              style: TextStyle(fontSize: fontSizeTitle),
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 8),
-                                Text('Date: ${data.date}',
-                                    style: TextStyle(fontSize: fontSizeBody)),
-                                Text('Lots: ${data.lots}',
-                                    style: TextStyle(fontSize: fontSizeBody)),
-                                Text('Total Arrived: ${data.totalArrived}',
-                                    style: TextStyle(fontSize: fontSizeBody)),
-                                Text('Quantity Sold: ${data.qtySold}',
-                                    style: TextStyle(fontSize: fontSizeBody)),
-                                SizedBox(height: 8),
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: 'Max Price: ',
-                                        style:
-                                            TextStyle(fontSize: fontSizeBody),
-                                      ),
-                                      TextSpan(
-                                        text: '${data.maxPrice}',
-                                        style: TextStyle(
-                                          fontSize: fontSizeBody,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.green,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: 'Average Price: ',
-                                        style:
-                                            TextStyle(fontSize: fontSizeBody),
-                                      ),
-                                      TextSpan(
-                                        text: '${data.avgPrice}',
-                                        style: TextStyle(
-                                          fontSize: fontSizeBody,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text('Type: ${data.type}',
-                                    style: TextStyle(fontSize: fontSizeBody)),
-                              ],
-                            ),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.share),
-                                  onPressed: () => _shareData(data),
-                                ),
-                                Icon(Icons.arrow_forward_ios,
-                                    color: Colors.teal),
-                              ],
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      CardamomDetailScreen(data: data),
-                                ),
-                              );
-                            },
-                          ),
+      body: Consumer<CardamomDataProvider>(
+        builder: (context, provider, child) {
+          if (provider.isLoading && provider.cardamomData.isEmpty) {
+            return Center(
+              child: Lottie.asset(
+                  "assets/animations/Animation - 1722503461417 (1).json"),
+            );
+          } else if (provider.cardamomData.isEmpty) {
+            return Center(child: Text('No data available'));
+          } else {
+            return RefreshIndicator(
+              onRefresh: _refreshData,
+              child: ListView.builder(
+                controller: _scrollController,
+                itemCount: provider.cardamomData.length +
+                    (provider.hasMoreData ? 1 : 0),
+                itemBuilder: (context, index) {
+                  if (index == provider.cardamomData.length) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  CardamomData data = provider.cardamomData[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: Card(
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(16),
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.teal,
+                          child: Icon(Icons.local_offer, color: Colors.white),
                         ),
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          );
+                        title: Text(
+                          'Auctioneer: ${data.auctioneer}',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 8),
+                            Text('Date: ${data.date}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Lots: ${data.lots}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Total Arrived: ${data.totalArrived}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Quantity Sold: ${data.qtySold}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Max Price: ${data.maxPrice}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Average Price: ${data.avgPrice}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Text('Type: ${data.type}',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                          ],
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.share),
+                          onPressed: () => _shareData(data),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    CardamomDetailScreen(data: data)),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
         },
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
   }
 }
